6f4e3169d02c04304571fd06095e4d818f4011e4
BarucCarrillo <baruccarrillo@outlook.com>
2025-12-26 21:18:20 -0700

    Creacion de roles de ADMIN y CLIENT

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
+
+//$role = Role::create(['name' => 'admin']); 
+//$role = Role::create(['name' => 'client']); 
 
 Route::get('/', function () {
     return view('welcome');
